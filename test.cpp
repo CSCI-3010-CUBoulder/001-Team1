eaf745b9fde4eb9272db9ac51be3a03f5ae7aaab
Kayden <kayden.adams@colorado.edu>
2020-02-10 17:28:28 -0700

    Adds dummy tests for odd and even mask

--- test.cpp
+++ test.cpp
@@ -1,2 +1,12 @@
 #define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do this in one cpp file
 #include "catch.hpp"
+#include "functions_to_implement.cpp"
+
+TEST_CASE( "EvenMask", "[masks]" ) {
+    REQUIRE( EvenMask(std::vector<int>{2, 4}); == std::vector<bool> {true, true}; );
+}
+
+TEST_CASE( "OddMask", "[masks]" ) {
+    REQUIRE( OddMask(std::vector<int>{2, 4}); == std::vector<bool> {false, false}; );
+
+}
